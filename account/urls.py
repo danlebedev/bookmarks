ee3735ef88e5e71fc7ca8e6aa0cb51dfaa92ec4c
danlebedev <lebedev.py@gmail.com>
2024-02-29 15:26:52 +0300

    Представления смены пароля

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,17 @@ urlpatterns = [
     # url-адреса входа и выхода.
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+    # url-адреса смены пароля.
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_views(),
+        name='password_change'
+    ),
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
+
     path('', views.dashboard, name='dashboard'),
 ]
