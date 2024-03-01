bb5597f10a0c25539323f63dd76b631b94a9ec1b
danlebedev <lebedev.py@gmail.com>
2024-03-01 15:24:55 +0300

    Создание миграций для модели профиля

--- account/views.py
+++ account/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, \
+    UserEditForm, ProfileEditForm
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -47,6 +49,8 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Сохранить объект User.
             new_user.save()
+            # Создать профиль пользователя.
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 'account/register_done.html',
@@ -58,4 +62,31 @@ def register(request):
         request,
         'account/register.html',
         {'user_form': user_form}
+    )
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST,
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES,
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form,
+        },
     )
