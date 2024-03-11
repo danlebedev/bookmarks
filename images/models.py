afebb7d225ae0f0495168fd84b2cdd0770411fd1
danlebedev <lebedev.py@gmail.com>
2024-03-11 12:26:15 +0300

    Разработка модели изображения 2

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -23,6 +24,11 @@ class Image(models.Model):
             models.Index(fields=['-created']),
         ]
         ordering = ['-created']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
