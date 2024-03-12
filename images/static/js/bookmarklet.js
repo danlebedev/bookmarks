062fd210d72a7ae3ea1f660c99127b29a4077454
danlebedev <lebedev.py@gmail.com>
2024-03-12 15:59:59 +0300

    change resolution picture

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,7 +1,7 @@
 const siteUrl = '//127.0.0.1:8000/';
 const styleUrl = siteUrl + 'static/css/bookmarklet.css';
-const minWidth = 250;
-const minHeight = 250;
+const minWidth = 200;
+const minHeight = 200;
 
 // Загрузить CSS.
 var head = document.getElementsByTagName('head')[0];
