416ef6f59f861c995072dce8915f153ff0967cd4
danlebedev <lebedev.py@gmail.com>
2024-03-18 12:48:23 +0300

    Добавление бесконечной постраничной прокрутки в список изображений

--- images/views.py
+++ images/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -66,3 +68,41 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.Get.get('page')
+    images_only = request.Get.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом,
+        # то доставить первую страницу.
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # Если AJAX-запрос и страница вне диапазона,
+            # то вернуть пустую страницу.
+            return HttpResponse('')
+        # Если страница вне диапазона,
+        # то вернуть последнюю страницу результатов.
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {
+                'section': 'images',
+                'images': images,
+            }
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {
+            'section': 'images',
+            'images': images,
+        }
+    )
