c3192521669dca10286e9e2956ae830a4eaef5ea
danlebedev <lebedev.py@gmail.com>
2024-03-21 10:57:00 +0300

    Создание шаблонов действий

--- account/views.py
+++ account/views.py
@@ -45,7 +45,8 @@ def dashboard(request):
         # Если пользователь подписан на других,
         # то извлечь только их действия.
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions.select_related('user', 'user__profile')[:10]
+    actions = actions.select_related('user', 'user__profile')[:10] \
+        .prefetch_related('target')[:10]
     return render(
         request,
         'account/dashboard.html',
