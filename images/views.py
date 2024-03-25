a29ac43571e251165435a67010d2624758b113a4
danlebedev <lebedev.py@gmail.com>
2024-03-25 15:36:11 +0300

    Хранение рейтинга в хранилище Redis

--- images/views.py
+++ images/views.py
@@ -56,6 +56,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # Увеличить общее число просмотров изображения на 1.
     total_views = r.incr(f'image:{image.id}:views')
+    # Увеличить рейтинг изображения на 1.
+    r.zincrby('image_ranking', 1, image.id)
     return render(
         request,
         'images/image/detail.html',
@@ -120,4 +122,21 @@ def image_list(request):
             'section': 'images',
             'images': images,
         }
+    )
+
+@login_required
+def image_ranking(request):
+    # Получить словарь рейтинга изображений.
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # Получить наиболее просматриваемые изображения.
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        'images/image/ranking.html',
+        {
+            'section': 'images',
+            'most_viewed': most_viewed,
+        },
     )
