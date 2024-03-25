d4545017ec1bab559d9624cb232e52fe06c44e96
danlebedev <lebedev.py@gmail.com>
2024-03-25 14:54:01 +0300

    Хранение просмотров изображений в хранилище Redis

--- images/views.py
+++ images/views.py
@@ -5,10 +5,19 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
+from django.conf import settings
 
 from .forms import ImageCreateForm
 from .models import Image
 from actions.utils import create_action
+import redis
+
+# Соединить с Redis.
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+)
 
 @login_required
 def image_create(request):
@@ -45,12 +54,15 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Увеличить общее число просмотров изображения на 1.
+    total_views = r.incr(f'image:{image.id}:views')
     return render(
         request,
         'images/image/detail.html',
         {
             'section': 'images',
             'image': image,
+            'total_views': total_views,
         }
     )
 
